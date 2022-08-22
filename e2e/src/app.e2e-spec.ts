090bb616f05ca1b0a5c04de7880593a709623203
Nithin Narayanan <nithin.narayanan.018@gmail.com>
2022-08-22 15:52:56 +0530

    three

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('BlogApp-using-Mean app is running!');
+    expect(page.getTitleText()).toEqual('Abode-using-Mean app is running!');
   });
 
   afterEach(async () => {
